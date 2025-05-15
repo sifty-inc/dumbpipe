2028c085e5d7882f74b6f460a30ee72630a6f252
Kevin Dinicola <kevinjdinicola@gmail.com>
2025-05-14 18:34:34 -0700

    updated

--- src/socks_server.rs
+++ src/socks_server.rs
@@ -1,29 +1,22 @@
-use fast_socks5::{
-    server::{Config, SimpleUserPassword, Socks5Server, Socks5Socket},
-    Result
-};
+use fast_socks5::server::{run_tcp_proxy, DnsResolveHelper, Socks5ServerProtocol};
+use fast_socks5::{ReplyError, Result, Socks5Command, SocksError};
 use std::future::Future;
-use tokio::io::{AsyncRead, AsyncWrite};
+use tokio::net::TcpListener;
 use tokio::task;
-use tokio_stream::StreamExt;
-use tracing::{info, warn};
+use tracing::{error, info, warn};
 
 pub const SOCKS_LISTEN_ADDR: &str = "127.0.0.1:1080";
 
 pub(crate) async fn spawn_socks_server() -> Result<()> {
-    let listener = <Socks5Server>::bind(SOCKS_LISTEN_ADDR).await?;
-    let listener = listener.with_config(Config::default());
-
-    let mut incoming = listener.incoming();
+    let listener = TcpListener::bind(SOCKS_LISTEN_ADDR).await?;
 
     info!("Listen for socks connections @ {}", SOCKS_LISTEN_ADDR);
 
     // Standard TCP loop
-    while let Some(socket_res) = incoming.next().await {
-        match socket_res {
-            Ok(socket) => {
-
-                spawn_and_log_error(socket.upgrade_to_socks5());
+    loop {
+        match listener.accept().await {
+            Ok((socket, _client_addr)) => {
+                spawn_and_log_error(serve_socks5(socket));
             }
             Err(err) => {
                 warn!("accept error = {:?}", err);
@@ -31,22 +24,38 @@ pub(crate) async fn spawn_socks_server() -> Result<()> {
         }
     }
 
+}
+
+const TIMEOUT: u64 = 30;
+async fn serve_socks5(socket: tokio::net::TcpStream) -> Result<(), SocksError> {
+    let (proto, cmd, target_addr) =
+        Socks5ServerProtocol::accept_no_auth(socket).await?
+        .read_command()
+        .await?
+        .resolve_dns()
+        .await?;
+
+    match cmd {
+        Socks5Command::TCPConnect => {
+            run_tcp_proxy(proto, &target_addr, TIMEOUT, false).await?;
+        }
+        _ => {
+            proto.reply_error(&ReplyError::CommandNotSupported).await?;
+            return Err(ReplyError::CommandNotSupported.into());
+        }
+    };
     Ok(())
 }
 
-fn spawn_and_log_error<F, T>(fut: F) -> task::JoinHandle<()>
+
+fn spawn_and_log_error<F>(fut: F) -> task::JoinHandle<()>
 where
-    F: Future<Output = Result<Socks5Socket<T, SimpleUserPassword>>> + Send + 'static,
-    T: AsyncRead + AsyncWrite + Unpin,
+    F: Future<Output = Result<()>> + Send + 'static,
 {
     task::spawn(async move {
         match fut.await {
-            Ok(mut socket) => {
-                if let Some(user) = socket.take_credentials() {
-                    info!("user logged in with `{}`", user.username);
-                }
-            }
-            Err(err) => warn!("{:#}", &err),
+            Ok(()) => {}
+            Err(err) => error!("{:#}", &err),
         }
     })
 }
--- start_remote_client.sh
+++ start_remote_client.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-./socks5 &
-trap "killall socks5" SIGTERM SIGINT
 
 exec ./dumbpipe listen-tcp --host 127.0.0.1:1080
